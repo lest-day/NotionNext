541b60a1fc659f82f567bf37b633041c63863467
lestday233 <106459514+lest-day@users.noreply.github.com>
2025-01-29 11:40:24 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -5,16 +5,5 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 export default function PoweredBy(props) {
-  return (
-    <div
-      className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
-      <span>Powered by</span>
-      <a
-        href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
-      .
-    </div>
-  )
+  return ()
 }
